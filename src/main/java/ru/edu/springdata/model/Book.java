f1c0a1069aace0b3e35077a8669eefce5d3f599d
RodinRoman <roman.v.rodin@gmail.com>
2024-04-19 05:23:15 +0300

    task jdbcTemplate

---
+++ src/main/java/ru/edu/springdata/model/Book.java
@@ -0,0 +1,9 @@
+package ru.edu.springdata.model;
+
+public class Book {
+
+    private Long id;
+    private String name;
+    private String language;
+    private String category; // history, it, health etc...
+}
