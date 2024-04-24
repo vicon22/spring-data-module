a154a137831e082b5296276b8861ec2c8d3207ce
evgenijisupov <z.isupov@gmail.com>
2024-04-25 00:22:10 +0300

    back

--- src/main/java/ru/edu/springdata/model/Book.java
+++ src/main/java/ru/edu/springdata/model/Book.java
@@ -1,12 +1,5 @@
 package ru.edu.springdata.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Book {
 
     private Long id;
