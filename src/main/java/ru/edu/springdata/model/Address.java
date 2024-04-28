8c77424731614e7e1e780cfb424394d078e89573
RodinRoman <roman.v.rodin@gmail.com>
2024-04-28 07:52:40 +0300

    second task

---
+++ src/main/java/ru/edu/springdata/model/Address.java
@@ -0,0 +1,8 @@
+package ru.edu.springdata.model;
+
+public class Address {
+
+    private Long id;
+    private String city;
+    private String street;
+}
---
+++ src/main/java/ru/edu/springdata/model/Author.java
@@ -0,0 +1,13 @@
+package ru.edu.springdata.model;
+
+/**
+ * Сущность описывающая авторов книг
+ */
+public class Author {
+
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String phone;
+}
